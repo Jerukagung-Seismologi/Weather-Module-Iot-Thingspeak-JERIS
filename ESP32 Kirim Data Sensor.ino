4712727ec8da4cbb15a3dcf504bdd96154a68fbd
Evan Alif Widhyatma <56740250+evanalif113@users.noreply.github.com>
2022-06-13 15:34:20 +0700

    Update ESP32 Kirim Data Sensor.ino
    Add output led indocator and BMP280 mode

--- ESP32 Kirim Data Sensor.ino
+++ ESP32 Kirim Data Sensor.ino
@@ -1,94 +1,102 @@
-
 #include <WiFi.h>
-#include "ThingSpeak.h"
+#include <WiFiClient.h>
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
-#include <Adafruit_BME280.h>
+#include <Adafruit_BMP280.h>
+#include "ThingSpeak.h"
 
-const char* ssid      = "XXXXXXXXXX"; // Nama SSID Jaringan
-const char* password  = "XXXXXXXXXX"; // Password SSID Jaringan
+const char* ssid      = "Replace with your network name";   
+const char* password  = "Replace with your network password";
 
-WiFiClient  client;
+WiFiClient client;
 
-unsigned long myChannelNumber = X; //Nomor Channel
-const char * myWriteAPIKey = "XXXXXXXXXXXX"; //Write API Channel
+unsigned long myChannelNumber = 1;
+const char* myWriteAPIKey = "REPLACE WITH YOUR WRITE KEY API";
 
 // Timer variables
 unsigned long lastTime = 0;
-/*Jeda dalam milidetik
-  2000 = 2 detik (30 data dalam 1 menit)
-  5000 = 5 detik (12 data dalam 1 menit)
-  15000 = 15 detik (4 data dalam 1 menit) WAJIB BILA LISENSI GRATIS UNTUK SAMPLING MAKS
-  30000 = 30 detik (2 data dalam 1 menit)
-*/
-unsigned long timerDelay = 15000; 
+unsigned long timerDelay = 14000; //MINIMAL 14 DETIK BIAR EFEKTIF
+const int ledStat = 2;
+const int ledWifi = 4;
+const int ledGood = 16;
+const int ledFail = 17;
 
-// Variable untuk menampilkan tipe data
+// Variable to hold temperature readings
 float temperature;
-float humidity;
 float pressure;
 
+// Create a sensor object
+Adafruit_BMP280 bmp;
 
-// Membuat objek data
-Adafruit_BME280 bme; //BME280 sambungkan ESP32 I2C (GPIO 21 = SDA, GPIO 22 = SCL)
-
-// Inisialisasi Sensor BME280
-void initBME(){
-  if (!bme.begin(0x76))
+void initBMP(){
+  if (!bmp.begin(0x76))
   {
-    Serial.println("Gagal Menemukan BME280, Mohon Cek Sambungan Kabel I2C");
+    Serial.println("Gagal Menemukan BMP280/BME280, Mohon Cek Sambungan Kabel I2C");
     while (1);
   }
+  bmp.setSampling(Adafruit_BMP280::MODE_FORCED,     /* Operating Mode. */
+                  Adafruit_BMP280::SAMPLING_X1,     /* Temp. oversampling */
+                  Adafruit_BMP280::SAMPLING_X1,    /* Pressure oversampling */
+                  Adafruit_BMP280::FILTER_OFF,      /* Filtering. */
+                  Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
 }
-// Program yang dimulai
+
+
 void setup() {
-  Serial.begin(115200);  //Inisialisasi Serial
-  initBME();
+  Serial.begin(115200);  //Initialize serial
+  initBMP();
   WiFi.mode(WIFI_STA);   
-  ThingSpeak.begin(client);  // Inisialisasi ThingSpeak
-  pinMode(LED_BUILTIN, OUTPUT);
+  ThingSpeak.begin(client);  // Initialize ThingSpeak
+  pinMode(ledStat, OUTPUT);
+  pinMode(ledWifi, OUTPUT);
+  pinMode(ledGood, OUTPUT);
+  pinMode(ledFail, OUTPUT);
 }
 
 void loop() {
   if ((millis() - lastTime) > timerDelay) {
-    
-    // Menyambungkan WiFi
-    if(WiFi.status() != WL_CONNECTED){
+    // Connect or reconnect to WiFi
+    if(WiFi.status() != WL_CONNECTED) {
+      digitalWrite(ledWifi, LOW);
       Serial.print("Melakukan Penyambungan Internet...");
       while(WiFi.status() != WL_CONNECTED){
         WiFi.begin(ssid, password); 
         delay(5000);     
-      } 
+      }
+      digitalWrite(ledWifi, HIGH);
       Serial.println("\nTersambung.");
     }
-
-    // Mengambil data sensor
-    digitalWrite(LED_BUILTIN, HIGH); // Indikator Mengawali Tugas
-    temperature = bme.readTemperature();
+    // Get a new temperature reading
+    digitalWrite(ledStat, HIGH);
+    temperature = bmp.readTemperature();
     Serial.print("Temperature (ºC): ");
     Serial.println(temperature);
-    humidity = bme.readHumidity();
-    Serial.print("Humidity (Rh%): ");
-    Serial.println(humidity);
-    pressure = bme.readPressure() / 100.0F;
+    /*humidity = bme.readHumidity();
+    Serial.print("Humidity (%): ");
+    Serial.println(humidity);*/
+    pressure = bmp.readPressure() / 100.0F;
     Serial.print("Pressure (hPa): ");
     Serial.println(pressure);
     
-    // Menampilkan variabel ke ThingSpeak
+    // set the fields with the values
     ThingSpeak.setField(1, temperature);
-    ThingSpeak.setField(2, humidity);
     ThingSpeak.setField(3, pressure);
     
-    // Menulis data ke Thingspeak
+    // Write to ThingSpeak. There are up to 8 fields in a channel, allowing you to store up to 8 different
+    // pieces of information in a channel.  Here, we write to field 1.
     int x = ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
 
     if(x == 200){
-      Serial.println("Field Berhasil Diperbaharui.");
+      digitalWrite(ledGood, HIGH);
+      digitalWrite(ledFail, LOW);
+      Serial.println("Channel Berhasil Diperbaharui. Kode HTTP" + String(x));
     }
     else{
-      Serial.println("Terjadi Masalah Pembaharuan Field. Kode Error HTTP " + String(x));
+      digitalWrite(ledFail, HIGH);
+      digitalWrite(ledGood, LOW);
+      Serial.println("Terjadi Masalah Pembaruan Channel. Kode HTTP" + String(x));
     }
     lastTime = millis();
-    digitalWrite(LED_BUILTIN, LOW); // Indikator Menyelesaikan Tugas
+    digitalWrite(ledStat, LOW);
   }
 }
